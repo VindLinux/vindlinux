5c340b489c88414568786610af34d641b524b5e3
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-09-07 15:35:16 -0300

    scripts: update fetch-sources and bootstrap script

--- scripts/01-fetch-sources.sh
+++ scripts/01-fetch-sources.sh
@@ -6,13 +6,52 @@
 # Existing packages are skipped.
 # Interrupted downloads are resumed.
 
+
+
 set -u
 
-mkdir sources/ && cd sources/
+# musl cross make
+
+mkdir -p sources/musl-cross-make-sources
+cd sources/musl-cross-make-sources
 
 wget -nc \
     https://mirrors.edge.kernel.org/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.xz \
     https://mirrors.edge.kernel.org/gnu/binutils/binutils-2.44.tar.gz \
     https://mirrors.edge.kernel.org/gnu/mpfr/mpfr-4.2.2.tar.xz \
     https://mirrors.edge.kernel.org/gnu/mpc/mpc-1.3.1.tar.gz \
-    https://mirrors.edge.kernel.org/gnu/gmp/gmp-6.3.0.tar.xz
+    https://mirrors.edge.kernel.org/gnu/gmp/gmp-6.3.0.tar.xz \
+    https://sources.voidlinux.org/musl-1.2.5/musl-1.2.5.tar.gz
+
+git clone https://github.com/richfelker/musl-cross-make
+
+mkdir -p sources/minimal-system
+
+cd sources/minimal-system
+
+wget -nc \
+    https://sources.voidlinux.org/musl-1.2.5/musl-1.2.5.tar.gz \
+    https://mirror.slackbuilds.org/slackware/slackware64-current/source/a/mkinitrd/busybox-1.37.0.tar.bz2 \
+    http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.12.tar.gz \
+    https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz \
+    https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz \
+    https://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.xz \
+    https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.79.tar.xz \
+    https://ftp.gnu.org/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.xz
+
+# lambda prerequisites
+
+mkdir -p "$VIND/usr/src"
+cd "$VIND/usr/src"
+
+wget -nc \
+    https://zlib.net/fossils/zlib-1.3.1.tar.gz \
+    https://www.cpan.org/src/5.0/perl-5.40.0.tar.gz \
+    https://www.openssl.org/source/openssl-3.5.7.tar.gz \
+    https://curl.se/ca/cacert.pem \
+    https://curl.se/download/curl-8.11.0.tar.gz \
+    https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz \
+    https://ftp.gnu.org/gnu/wget/wget-1.24.5.tar.gz \
+    https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-1.7.1.tar.gz \
+    https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.47.0.tar.xz \
+    https://ftp.gnu.org/gnu/gettext/gettext-0.22.5.tar.gz \
---
+++ scripts/02-bootstrap.sh
@@ -0,0 +1,195 @@
+#!/bin/sh
+
+source ./vind-config.sh
+source ./vind-utils.sh
+
+mkdir -p "$MARKERS" || {
+    error "Failed to create markers directory"
+    exit 1
+}
+
+# resolv.conf
+
+if [ ! -f "$MARKERS/.resolv_done" ]; then
+    info "Configuring resolv.conf"
+
+    cat > /etc/resolv.conf << 'EOF'
+nameserver 1.1.1.1
+nameserver 8.8.8.8
+EOF
+
+    if [ $? -ne 0 ]; then
+        error "Failed to configure resolv.conf"
+        exit 1
+    fi
+
+    touch "$MARKERS/.resolv_done"
+fi
+
+# rootfs
+
+if [ ! -f "$MARKERS/.rootfs_done" ]; then
+    info "Creating rootfs"
+
+    mkdir -pv "$VIND"/{etc,var} || {
+        error "Failed to create rootfs directories"
+        exit 1
+    }
+
+    mkdir -pv "$VIND"/usr/{bin,lib,sbin,include,share,src} || {
+        error "Failed to create /usr directories"
+        exit 1
+    }
+
+    for i in bin lib sbin; do
+        ln -sv usr/"$i" "$VIND"/"$i" || {
+            error "Failed to create /$i symlink"
+            exit 1
+        }
+    done
+
+    mkdir -pv "$VIND"/{dev,proc,sys,run,tmp,home,root,mnt,opt} || {
+        error "Failed to create rootfs directories"
+        exit 1
+    }
+
+    chmod 1777 "$VIND/tmp" || {
+        error "Failed to chmod $VIND/tmp"
+        exit 1
+    }
+
+    chmod 1777 "$VIND/var/tmp" || {
+        error "Failed to chmod $VIND/var/tmp"
+        exit 1
+    }
+
+    touch "$MARKERS/.rootfs_done"
+fi
+
+# musl-cross-make
+
+if [ ! -f "$MARKERS/.musl-cross-make_done" ]; then
+    info "Building musl-cross-make"
+
+    mkdir -p "$VIND/tools-src" || {
+        error "Failed to create tools-src"
+        exit 1
+    }
+
+    cd "$VIND/tools-src" || {
+        error "Failed to enter tools-src"
+        exit 1
+    }
+
+    mv $SOURCES/musl-cross-make-sources/musl-cross-make . || {
+        error "Failed to move musl-cross-make"
+        exit 1
+    }
+
+    cd musl-cross-make || {
+        error "Failed to enter musl-cross-make"
+        exit 1
+    }
+
+    export TOOLS="$VIND/tools"
+
+    cat > config.mak << EOF
+TARGET = x86_64-pc-linux-musl
+OUTPUT = $TOOLS
+GCC_VER = 13.3.0
+EOF
+
+    if [ $? -ne 0 ]; then
+        error "Failed to create config.mak"
+        exit 1
+    fi
+
+    mkdir -p sources || {
+        error "Failed to create sources directory"
+        exit 1
+    }
+
+    mv $SOURCES/musl-cross-make-sources/* sources/ || {
+        error "Failed to move musl-cross-make sources"
+        exit 1
+    }
+
+    touch sources/* || {
+        error "Failed to touch musl-cross-make sources"
+        exit 1
+    }
+
+    make -j$(nproc) || {
+        error "musl-cross-make build failed"
+        exit 1
+    }
+
+    make install || {
+        error "musl-cross-make installation failed"
+        exit 1
+    }
+
+    ln -sf "$TOOLS/bin/x86_64-pc-linux-musl-ar" /usr/bin/ar || {
+        error "Failed to create ar symlink"
+        exit 1
+    }
+
+    ln -sf "$TOOLS/bin/x86_64-pc-linux-musl-ranlib" /usr/bin/ranlib || {
+        error "Failed to create ranlib symlink"
+        exit 1
+    }
+
+    ln -sf "$TOOLS/bin/x86_64-pc-linux-musl-strip" /usr/bin/strip || {
+        error "Failed to create strip symlink"
+        exit 1
+    }
+
+    touch "$MARKERS/.musl-cross-make_done"
+fi
+
+# minimal system
+
+# musl
+
+if [ ! -f "$MARKERS/.musl_done" ]; then
+    info "Building musl"
+
+    mv $SOURCES/minimal-system/musl-1.2.5.tar.gz "$VIND/sources" || {
+        error "Failed to move musl source"
+        exit 1
+    }
+
+    cd "$VIND/sources" || {
+        error "Failed to enter sources directory"
+        exit 1
+    }
+
+    tar -xf musl-1.2.5.tar.gz || {
+        error "Failed to extract musl"
+        exit 1
+    }
+
+    cd musl-1.2.5 || {
+        error "Failed to enter musl source directory"
+        exit 1
+    }
+
+    ./configure --prefix="$PREFIX" --syslibdir="$PREFIX/lib" || {
+        error "musl configure failed"
+        exit 1
+    }
+
+    make $MAKEOPTS || {
+        error "musl build failed"
+        exit 1
+    }
+
+    make DESTDIR="$DESTDIR" install || {
+        error "musl installation failed"
+        exit 1
+    }
+
+    touch "$MARKERS/.musl_done"
+fi
+```
+
