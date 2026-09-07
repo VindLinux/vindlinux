d0f36de36851d2dd21dfe8ba95ba352e8ab9fb82
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-09-07 19:16:49 -0300

    scripts(01-fetch-sources): source vind-config and use wget -c to resume downloads

--- scripts/01-fetch-sources.sh
+++ scripts/01-fetch-sources.sh
@@ -6,16 +6,23 @@
 # Existing packages are skipped.
 # Interrupted downloads are resumed.
 
-
-
 set -u
+
+source ./vind-config.sh
+source ./vind-utils.sh
 
 # musl cross make
 
-mkdir -p sources/musl-cross-make-sources
-cd sources/musl-cross-make-sources
+mkdir -p sources/musl-cross-make-sources || {
+    error "Failed to create sources/musl-cross-make-sources"
+    exit 1
+}
+cd sources/musl-cross-make-sources || {
+    error "Failed to enter sources/musl-cross-make-sources"
+    exit 1
+}
 
-wget -nc \
+wget -c \
     https://mirrors.edge.kernel.org/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.xz \
     https://mirrors.edge.kernel.org/gnu/binutils/binutils-2.44.tar.gz \
     https://mirrors.edge.kernel.org/gnu/mpfr/mpfr-4.2.2.tar.xz \
@@ -24,15 +31,27 @@ wget -nc \
     https://sources.voidlinux.org/musl-1.2.5/musl-1.2.5.tar.gz \
     https://ftp.barfooze.de/pub/sabotage/tarballs/linux-headers-4.19.88-2.tar.xz
 
-git clone https://github.com/richfelker/musl-cross-make
+git clone https://github.com/richfelker/musl-cross-make || {
+    error "Failed to clone musl-cross-make"
+    exit 1
+}
 
-cd ../..
+cd ../.. || {
+    error "Failed to return to sources directory"
+    exit 1
+}
 
-mkdir -p sources/minimal-system
+mkdir -p sources/minimal-system || {
+    error "Failed to create sources/minimal-system"
+    exit 1
+}
 
-cd sources/minimal-system
+cd sources/minimal-system || {
+    error "Failed to enter sources/minimal-system"
+    exit 1
+}
 
-wget -nc \
+wget -c \
     https://sources.voidlinux.org/musl-1.2.5/musl-1.2.5.tar.gz \
     https://mirror.slackbuilds.org/slackware/slackware64-current/source/a/mkinitrd/busybox-1.37.0.tar.bz2 \
     http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.12.tar.gz \
@@ -44,10 +63,16 @@ wget -nc \
 
 # lambda prerequisites
 
-mkdir -p "$VIND/usr/src"
-cd "$VIND/usr/src"
+mkdir -p "$VIND/usr/src" || {
+    error "Failed to create $VIND/usr/src"
+    exit 1
+}
+cd "$VIND/usr/src" || {
+    error "Failed to enter $VIND/usr/src"
+    exit 1
+}
 
-wget -nc \
+wget -c \
     https://zlib.net/fossils/zlib-1.3.1.tar.gz \
     https://www.cpan.org/src/5.0/perl-5.40.0.tar.gz \
     https://www.openssl.org/source/openssl-3.5.7.tar.gz \
@@ -57,4 +82,4 @@ wget -nc \
     https://ftp.gnu.org/gnu/wget/wget-1.24.5.tar.gz \
     https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-1.7.1.tar.gz \
     https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.47.0.tar.xz \
-    https://ftp.gnu.org/gnu/gettext/gettext-0.22.5.tar.gz \
+    https://ftp.gnu.org/gnu/gettext/gettext-0.22.5.tar.gz
